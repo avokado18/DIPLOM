5583995acca48c71eaedcb9d638e43a56e17393c
proskuryakova <vproskuryakova@cinimex.ru>
2017-06-03 20:26:18 +0300

    start security

---
+++ src/main/java/com/diplom/backend/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.diplom.backend;
+
+import com.diplom.backend.security.CustomAuthenticationProvider;
+import com.diplom.backend.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = CustomUserDetailsService.class)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private CustomAuthenticationProvider authProvider;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().anyRequest().authenticated();
+        http
+                .formLogin().defaultSuccessUrl("/")
+                .usernameParameter("username").passwordParameter("password")
+                .and()
+                .logout().logoutSuccessUrl("/login")
+                .and()
+                .csrf();
+    }
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authProvider);
+    }
+}
---
+++ src/main/java/com/diplom/backend/security/CustomAuthenticationProvider.java
@@ -0,0 +1,60 @@
+package com.diplom.backend.security;
+
+import com.dsr.myProject.dto.UserDTO;
+import com.dsr.myProject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    @Autowired
+    private UserService userService;
+
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String name = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        UserDTO user = userService.findByName(name);
+        if ((user != null) && (password.equals(user.getPassword()) && (checkIs18yearsOld(user.getBirthdate())))) {
+            return new UsernamePasswordAuthenticationToken(
+                    name, password, new ArrayList<GrantedAuthority>());
+        } else {
+            return null;
+        }
+    }
+
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(
+                UsernamePasswordAuthenticationToken.class);
+    }
+
+    private boolean checkIs18yearsOld(long date){
+        Date now = new Date();
+        Date birthdate = new Date(date);
+        if (now.getYear() - birthdate.getYear() < 18)
+            return false;
+        else
+            if (now.getYear() - birthdate.getYear() > 18)
+                return true;
+            else {
+                if (now.getMonth() > birthdate.getMonth())
+                    return true;
+                else
+                    if (now.getMonth() < birthdate.getMonth())
+                        return false;
+                    else {
+                        if (now.getDate() >= birthdate.getDate())
+                            return true;
+                        else
+                            return false;
+                    }
+            }
+    }
+}
